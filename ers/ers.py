1fc46ea608368cbda0361c69e9bfcbffba135063
Cristian Dinu <goc9000@gmail.com>
2013-04-11 19:11:46 +0200

    Minor bugfix

--- ers/ers.py
+++ ers/ers.py
@@ -144,7 +144,7 @@ class ERSLocal(ERSReadWrite):
     def __init__(self, server_url=r'http://admin:admin@127.0.0.1:5984/', dbname='ers', model=DEFAULT_MODEL,
                 fixed_peers=()):
         super(ERSLocal, self).__init__(server_url, dbname, model)
-        self.fixed_peers = fixed_peers
+        self.fixed_peers = list(fixed_peers)
 
     def get_annotation(self, entity):
         result = self.get_data(entity)
