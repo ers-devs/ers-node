e864b4359cc335b3c792e7c15a75d6a01f1011e5
Cristian Dinu <goc9000@gmail.com>
2013-05-02 14:47:38 +0200

    Moved N-triples parsing functionality to EntityCache itself

--- ers/ers.py
+++ ers/ers.py
@@ -3,6 +3,8 @@
 import couchdbkit
 import rdflib
 import peer_monitor
+
+from StringIO import StringIO
 from collections import defaultdict
 from models import ModelS, ModelT
                                                         
@@ -23,6 +25,46 @@ class EntityCache(defaultdict):
     def add(self, s, p, o):
         """Add <s, p, o> to cache."""
         self[s][p].add(o)
+
+    def parse_nt(self, **kwargs):
+        if 'filename' in kwargs:
+            lines = open(kwargs['filename'], 'r')
+        elif 'data' in kwargs:
+            lines = StringIO(kwargs['data'])
+        else:
+            raise RuntimeError("Must specify filename= or data= for parse_nt")
+
+        for input_line in lines:
+            triple = input_line.split(None, 2) # assumes SPO is separated by any whitespace string with leading and trailing spaces ignored
+            s = triple[0][1:-1] # get rid of the <>, naively assumes no bNodes for now
+            p = triple[1][1:-1] # get rid of the <>
+            o = triple[2][1:-1] # get rid of the <> or "", naively assumes no bNodes for now
+            oquote = triple[2][0]
+            if oquote == '"':
+                o = triple[2][1:].rsplit('"')[0]
+            elif oquote == '<':
+                o = triple[2][1:].rsplit('>')[0]
+            else:
+                o = triple[2].split(' ')[0] # might be a named node
+
+            self.add(s, p, o)
+
+        return self
+
+    def parse_nt_rdflib(self, **kwargs):
+        graph = rdflib.Graph()
+
+        if 'filename' in kwargs:
+            graph.parse(location=kwargs['filename'], format='nt')
+        elif 'data' in kwargs:
+            graph.parse(data=kwargs['data'], format='nt')
+        else:
+            raise RuntimeError("Must specify filename= or data= for parse_nt_rdflib")
+
+        for s, p, o in graph:
+            self.add(str(s), str(p), str(o))
+
+        return self
 
 
 class ERSReadOnly(object):
@@ -99,31 +141,15 @@ class ERSReadWrite(ERSReadOnly):
 
     def import_nt(self, file_name, target_graph):
         """Import N-Triples file."""
-        cache = EntityCache()
-        input_doc = open(file_name, "r")
-        for input_line in input_doc:
-            triple = input_line.split(None, 2) # assumes SPO is separated by any whitespace string with leading and trailing spaces ignored
-            s = triple[0][1:-1] # get rid of the <>, naively assumes no bNodes for now
-            p = triple[1][1:-1] # get rid of the <>
-            o = triple[2][1:-1] # get rid of the <> or "", naively assumes no bNodes for now
-            oquote = triple[2][0]
-            if oquote == '"':
-                o = triple[2][1:].rsplit('"')[0]
-            elif oquote == '<':
-                o = triple[2][1:].rsplit('>')[0]
-            else:
-                o = triple[2].split(' ')[0] # might be a named node
-            cache.add(s, p, o)
+        cache = EntityCache().parse_nt(filename=file_name)
+
         self.write_cache(cache, target_graph)
 
     def import_nt_rdflib(self, file_name, target_graph):
         """Import N-Triples file using rdflib."""
         # TODO: get rid of the intermediate cache?
-        cache = EntityCache()
-        graph = rdflib.Graph()
-        graph.parse(file_name, format='nt')
-        for s, p, o in graph:
-            cache.add(str(s), str(p), str(o))
+        cache = EntityCache().parse_nt_rdflib(filename=file_name)
+
         self.write_cache(cache, target_graph)
 
     def write_cache(self, cache, graph):
