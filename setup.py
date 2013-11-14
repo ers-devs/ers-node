c9e920be60212f73c9b0eb9e85d7f39ee8e7594b
Marat Charlaganov <charlaganov@gmail.com>
2013-11-14 16:15:56 +0100

    Update installation script

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-from os import chdir
 
 requirements = open('requirements.txt').read().splitlines()
-chdir('ers-local')
 setup(name='ERS',
       version='0.12',
       description='Entity Registry System',
-      url='https://github.com/ers-devs/ers/',
+      url='https://github.com/ers-devs/ers-node/',
       packages=['ers'],
-      requires = requirements,
-      test_suite = "ers.ers.test"
+      requires = requirements
      )
