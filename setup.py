25955ef90418b2e9452fffbbb31133d4919c64db
Marat Charlaganov <charlaganov@gmail.com>
2013-11-14 16:24:49 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 requirements = open('requirements.txt').read().splitlines()
 setup(name='ERS',
-      version='0.12',
+      version='0.13',
       description='Entity Registry System',
       url='https://github.com/ers-devs/ers-node/',
       packages=['ers'],
