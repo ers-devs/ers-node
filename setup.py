339d7823771f75ead6546fff70ef61f274c1dc3d
Christophe Gueret <christophe.gueret@gmail.com>
2014-08-03 08:53:49 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 requirements = open('requirements.txt').read().splitlines()
 setup(name='ERS',
-      version='0.16',
+      version='0.16', 
       description='Entity Registry System',
       url='https://github.com/ers-devs/ers-node/',
       packages=['ers'],
