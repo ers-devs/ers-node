fc17e55d16e402176754d7ece5745b4c684e5e7b
Mihai Gramada <gramada.mihai@gmail.com>
2015-07-05 11:09:36 +0200

    flask is now multithreaded, added api calls to start / stop the ers daemon, added new db query

--- web_api.py
+++ web_api.py
@@ -2,13 +2,17 @@
     Web api that can be used to interact from the exterior with the ERS nodes.
     ! preserves state
 '''
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
+import concurrent.futures
 
 from ers import ERS
 from ers import store
 from cli_interface import *
 import json
+import os
+import subprocess
+import signal
 
 interface = Interface()
 TEST_ENTITY = "urn:ers:test"
@@ -16,6 +20,34 @@ TEST_ENTITY = "urn:ers:test"
 @app.route('/')
 def welcome():
     return 'ERS web interface running with host uri %s' % interface.ers.get_machine_uri()
+
+@app.route('/DaemonRunning/')
+def check_daemon_running():
+    if os.path.isfile('/vagrant/ers_daemon.pid'):
+        f = open('/vagrant/ers_daemon.pid')
+        lines = f.readlines()
+        pid = lines[0].split()[0]
+        return str(pid)
+    else:
+        return '0'
+
+@app.route('/StartDaemon')
+def start_daemon():
+    command = 'python /vagrant/ers-node/ers/daemon.py --config /etc/ers-node/ers-node.ini'
+    pid = subprocess.Popen(command.split()).pid
+    return str(pid)
+
+@app.route('/StopDaemon/<pid>')
+def stop_daemon(pid):
+    # linux, mac
+    os.kill(int(pid), signal.SIGTERM)
+    try:
+        killedpid, stat = os.waitpid(pid, os.WNOHANG)
+        if killedpid == 0:
+            return 'process failed to kill'
+    except:
+        return 'process killed'
+
 
 @app.route('/ServiceStoreAccess')
 def service_store_access():
@@ -35,12 +67,28 @@ def get_entity(entity):
     created_entity = interface.ers.get(entity, include_remote = True)
     return 'got %s !\n' % entity
 
+@app.route('/BatchAddStatement/<entity>/', methods = ['POST'])
+def batch_add_statement(entity):
+    request_data = json.loads(request.data)
+    predicates = request_data['predicates']
+    values = request_data['values']
+
+
+    for i in range(len(predicates)):
+        pred = predicates[i]
+        val = values[i]
+        add_statement(entity, pred, val)
+    return 'batch added'
+
 @app.route('/AddStatement/<entity>/<predicate>/<value>')
 def add_statement(entity, predicate, value):
     ers_entity = interface.ers.get(entity, include_remote = False)
     ers_entity.add(predicate, value)
     interface.ers.persist_entity(ers_entity)
-    return 'added public %s:%s:%s to ERS!\n' %(entity, predicate, value)
+
+    document_id = ers_entity._documents['public']._doc['_id']
+
+    return 'added public %s:%s:%s to ERS with id %s !\n' %(entity, predicate, value, str(document_id))
 
 @app.route('/CacheEntity/<entity>')
 def cache_entity(entity):
@@ -68,7 +116,13 @@ def list_peers():
 def show_cache(db_name):
     entries = interface.ers.store[db_name].view('_all_docs').rows
 
-    return str(entries)
+@app.route('/ShowDoc/<db_name>/<doc_id>/<property_id>')
+def show_doc(db_name, doc_id, property_id):
+    try:
+        entries = interface.ers.store[db_name][doc_id][property_id]
+        return str(len(entries))
+    except:
+        return '0'
 
 def decode_value(encoded_value, encoded_type):
     value = encoded_value
@@ -115,4 +169,4 @@ def search(prop, val):
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', threaded=True)
